23dd75d7988f1142f46ddff1801d15add4717b37
CoolD69 <parthasingharoy166@gmail.com>
2022-09-01 11:09:16 +0530

    ok

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
-//ES6 Syntax
-//import express from 'express'
-//import {updateESP} from './MQTT/mqtt'
-//ES5 Syntax
 const express = require('express')
+const cors = require('cors')
 const updateESP = require('./middlewares/mqtt')
+const authenticate = require('./middlewares/authenticate')
+require('dotenv').config()
+
 
 const app = express()
 const PORT = process.env.PORT ||5000
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
-
-app.get('/lol', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.urlencoded({extended:false})) //all the form info available
+app.set('view-engine','ejs')
 
 //coming post request from client to update the codes
 app.post('/updateesp',updateESP,(req,res)=>{
-    res.json({
-        status:true,
-        message:"success"
-    })
+  res.json({status:true,message:"success",publish:true})
 })
 
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
+app.post('/upload',(req,res)=>{
+  
 })
 
+app.listen(PORT, () => {console.log(`Server listening on port ${PORT}`)})
 
+app.post('/auth',function(req, res) {
+  try{
+    let username = req.body.username; let password = req.body.password;
+    if (password === process.env.PASSWORD && username === process.env.USER_NAME) {
+
+        res.render('Admin.ejs')
+    }
+    else {
+      res.render('alert.ejs')
+    }
+}
+catch(err){console.log(err); }
+});
 
 
 
