19baad5ce4085da215afd1652facdfb031ba43d3
CoolD69 <parthasingharoy166@gmail.com>
2022-09-03 23:51:47 +0530

    ESPUpadate Function Added

--- ESP8266/src/main.cpp
+++ ESP8266/src/main.cpp
@@ -19,12 +19,14 @@ char msg[MSG_BUFFER_SIZE];
 // Create a random client ID
 String clientId = "ESP" + String(ESP.getChipId());
 
-
-
-// https://github.com/tzapu/WiFiManager
+// Function Prototype:
 void wifi_setup();
 void callback(char *topic, byte *payload, unsigned int length);
 void reconnect();
+void update_started();
+void update_finished();
+void update_progress(int cur, int total);
+void update_error(int err);
 
 void setup()
 {
@@ -38,16 +40,17 @@ void setup()
     client.setServer(mqtt_server, 1883);
     client.setClient(espClient);
     client.setCallback(callback);
+    digitalWrite(BUILTIN_LED,HIGH);
 }
 
 void loop()
 {
     // put your main code here, to run repeatedly:
-    if (!client.connected()){reconnect();
+    if (!client.connected())
+    {
+        reconnect();
     }
     client.loop();
-
-   
 }
 
 void wifi_setup()
@@ -71,7 +74,6 @@ void wifi_setup()
     }
 }
 
-
 void callback(char *topic, byte *payload, unsigned int length)
 {
     // debug purpose
@@ -84,7 +86,12 @@ void callback(char *topic, byte *payload, unsigned int length)
     if ((char)payload[0] == '1')
     {
         digitalWrite(BUILTIN_LED, LOW); // Turn the LED on (Note that LOW is the voltage level
-                                        // call the update function
+          while(1){
+            Serial.println(F("Updating..."));
+            // call the update function;
+            ESPUpdate(); 
+            Serial.println(F("Done..."));
+        }
     }
     else
     {
@@ -115,6 +122,56 @@ void reconnect()
     }
 }
 
+void ESPUpdate()
+{
+    
 
+        ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
 
+        // Add optional callback notifiers
+        ESPhttpUpdate.onStart(update_started);
+        ESPhttpUpdate.onEnd(update_finished);
+        ESPhttpUpdate.onProgress(update_progress);
+        ESPhttpUpdate.onError(update_error);
 
+        // t_httpUpdate_return ret = ESPhttpUpdate.update(client, '192.168.43.73', 80, "/blink");
+        // t_httpUpdate_return ret = ESPhttpUpdate.update(client,"192.168.43.73", 80, "/blink");
+        t_httpUpdate_return ret = ESPhttpUpdate.update(espClient, "192.168.43.73", 80, "/blink");
+
+        switch (ret)
+        {
+        case HTTP_UPDATE_FAILED:
+            Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+            break;
+
+        case HTTP_UPDATE_NO_UPDATES:
+            Serial.println(F("HTTP_UPDATE_NO_UPDATES"));
+            break;
+
+        case HTTP_UPDATE_OK:
+            Serial.println(F("HTTP_UPDATE_OK"));
+            break;
+        }
+    
+}
+
+// callback functions
+void update_started()
+{
+    Serial.println(F("CALLBACK:  HTTP update process started"));
+}
+
+void update_finished()
+{
+    Serial.println(F("CALLBACK:  HTTP update process finished"));
+}
+
+void update_progress(int cur, int total)
+{
+    Serial.printf("CALLBACK:  HTTP update process at %d of %d bytes...\n", cur, total);
+}
+
+void update_error(int err)
+{
+    Serial.printf("CALLBACK:  HTTP update fatal error code %d\n", err);
+}
