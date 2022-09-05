5b239dee9eea0f29baf9adc746d532d1d03e1cab
CoolD69 <parthasingharoy166@gmail.com>
2022-09-05 22:51:45 +0530

    initial commit

---
+++ APP/src/App.js
@@ -0,0 +1,43 @@
+import React,{useContext} from 'react';
+import './App.css';
+
+function App() {
+  const themes = {
+    light: {
+      foreground: "#000000",
+      background: "#eeeeee"
+    },
+    dark: {
+      foreground: "#ffffff",
+      background: "#222222"
+    }
+  };
+  
+  const ThemeContext = React.createContext(themes.dark);
+  
+    return (
+      <ThemeContext.Provider value={themes.dark}>
+        <Toolbar />
+      </ThemeContext.Provider>
+    );
+  
+  
+  function Toolbar(props) {
+    return (
+      <div>
+        <ThemedButton />
+      </div>
+    );
+  }
+  
+  function ThemedButton() {
+    const theme = useContext(ThemeContext);
+    return (
+      <button style={{ background: theme.background, color: theme.foreground }}>
+        I am styled by theme context!
+      </button>
+    );
+  }
+}
+
+export default App;
